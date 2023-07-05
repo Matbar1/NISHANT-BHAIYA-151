e06f6416318e99b0caa9d89614cb9a6c5d9a438d
Matbar1 <136784491+Matbar1@users.noreply.github.com>
2023-07-05 10:45:14 +0530

    Add files via upload

---
+++ Check Arithmetic Progression.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+      bool checkIsAP(int arr[], int n)
+    {
+      if (n <=2)
+        return true;
+     
+      sort(arr, arr + n);
+     
+      int d = arr[1] - arr[0];
+      for (int i=2; i<n; i++)
+        if (arr[i] - arr[i-1] != d)
+          return false;
+     
+      return true;
+    }
+ 
+
+};
